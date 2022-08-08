b41238fb0249de145c25a530105992067a6b544e
Kotalelabi <103073008+Kotalelabi@users.noreply.github.com>
2022-08-08 00:14:33 +0000

    "C:\Users\hp\Desktop\Me\assignment2.cpp"

---
+++ assignment2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+// Kota Lelabi-10985440
+int main()
+{
+ int sumOFNumbers =0;
+ int counter;
+      int count= 0;
+      for(int i= 2;  i<number; i++){
+          int counter =0;
+          for (int j =1; j<=i; j++){
+              if(i%j==0){
+                  counter++;
+              }
+          }
+          if(counter == 2){
+              count++;
+              cout <<sumOFNumbers += i;
+          }
+
+         }
+    return 0;
+}
